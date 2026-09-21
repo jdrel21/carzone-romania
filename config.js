b307f08a412dea16505b50cc793dfeda9b884e96
jdrel21 <smedoiudarius@gmail.com>
2026-09-21 17:39:03 +0200

    Refactor Supabase config to use constants

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.CARZONE_CONFIG = {
-  SUPABASE_URL: "https://donqfqhhwksgxhdtglxs.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_7VNStKgqFfyusvShZzd5Cg_J_dPgXSK"
-};
+const SUPABASE_URL = "https://dondfqhhwksgxhdtglxs.supabase.co";
+
+const SUPABASE_ANON_KEY =
+  "sb_publishable_7VNStKgqFfyusvShZzd5Cg_J_dPgXSK";
